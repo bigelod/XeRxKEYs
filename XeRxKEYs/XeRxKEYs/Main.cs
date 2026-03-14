28a249644351c8a6accd8f6440cca526b3909c2d
Daven Bigelow <davenbigelow92@gmail.com>
2026-03-14 15:58:58 -0400

    First commit
    Initial planning and design for this project

---
+++ XeRxKEYs/XeRxKEYs/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XeRxKEYs
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void niTaskbarIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
