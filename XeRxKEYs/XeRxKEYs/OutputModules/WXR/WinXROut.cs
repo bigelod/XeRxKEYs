0a37d37333ba1e49beaee019969e90a1da0eb82e
Daven Bigelow <davenbigelow92@gmail.com>
2026-03-18 22:48:06 -0400

    Updated WinXR Out to start InputAPI
    InputAPI needs a file to start on WinlatorXR

--- XeRxKEYs/XeRxKEYs/OutputModules/WXR/WinXROut.cs
+++ XeRxKEYs/XeRxKEYs/OutputModules/WXR/WinXROut.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -14,13 +15,57 @@ namespace XeRxKEYs.OutputModules.WXRModule
         public string DisplayName { get; set; }
 
         private int udpPortOUT = 7728;
+        private int udpPortIN = 7287;
         private string targetIP = "127.0.0.1";
 
         private UdpClient transmitClient;
 
+        private string dataDirectory = "Z:/tmp/xr";
+
         public void Setup()
         {
             transmitClient = new UdpClient();
+
+            if (dataDirectory == "Z:/tmp/xr" && !Directory.Exists(dataDirectory))
+            {
+                if (Directory.Exists("Z:/"))
+                {
+                    //Try to create the temp directory if it doesn't exist
+                    if (!Directory.Exists("Z:/tmp"))
+                    {
+                        Directory.CreateDirectory("Z:/tmp");
+                    }
+                }
+                else
+                {
+                    //Fallback to a temp directory on whatever drive we are running on instead
+                    dataDirectory = Path.GetFullPath("./").Substring(0, 1) + ":/xrtemp";
+                }
+            }
+
+            if (!Directory.Exists(dataDirectory)) Directory.CreateDirectory(dataDirectory);
+
+            if (!Directory.Exists(dataDirectory))
+            {
+                return;
+            }
+
+            string inputVersionFile = dataDirectory + "/input_version";
+
+            if (!File.Exists(inputVersionFile))
+            {
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(inputVersionFile))
+                    {
+                        sw.WriteLine("0.1");
+                    }
+                }
+                catch
+                {
+
+                }
+            }
         }
         public void Shutdown()
         {
